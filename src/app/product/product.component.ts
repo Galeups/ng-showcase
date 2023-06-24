7f0e0ee88b7932e7d60373bd818fc809b496ba27
Galeups <artdevice@gmail.com>
2023-06-24 23:20:58 +0200

    Add product card

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,12 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Product, ProductForm } from './models/product';
+import { Router } from '@angular/router';
+import { ProductService } from './services/product.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [ProductService],
 })
 export class ProductComponent implements OnInit {
-  constructor() {}
+  @Input() set product(product: Product) {
+    this.form.setValue(product);
+  }
+
+  readonly form = this.fb.group<Partial<ProductForm>>({
+    title: '',
+    description: '',
+  });
+
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly router: Router,
+    private readonly productService: ProductService
+  ) {}
 
   ngOnInit(): void {}
+
+  onCancel() {
+    this.router.navigate(['/']);
+  }
+
+  onSubmit() {
+    this.productService.submit(this.form.value);
+  }
 }
