f216c763854d1c39ca632fafc7083e6e923cd2c5
Galeups <artdevice@gmail.com>
2023-06-24 21:08:21 +0200

    Add product item

---
+++ src/app/products-list/components/product-item/product-item.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
